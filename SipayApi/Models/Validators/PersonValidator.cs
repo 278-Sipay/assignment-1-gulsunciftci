649427abd7918232554fcb04ae2692ff49a4984e
Gulsun Ciftci <ciftcigulsun8@gmail.com>
2023-07-15 16:02:06 +0300

    Sipay Bootcamp Restful Api Hafta 1 kısmı tamamlandı

---
+++ SipayApi/Models/Validators/PersonValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using System;
+
+namespace SipayApi.Models.Validators
+{
+    public class PersonValidator : AbstractValidator<Person> //Hangi sınıfın validator'ı olduğunu göstermek için o sınıfın AbstractValidator'ının inherit aldım 
+    {
+        public PersonValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().NotNull().WithMessage("Name cannot be empty");
+            RuleFor(x => x.Name).MinimumLength(5).MaximumLength(100).WithMessage("Name length can be at least 5 and up to 100"); ;
+
+
+            RuleFor(x => x.LastName).NotEmpty().NotNull().WithMessage("LastName cannot be empty");
+            RuleFor(x => x.LastName).MinimumLength(5).MaximumLength(100).WithMessage("Lastname length can be at least 5 and up to 100");
+
+            RuleFor(x => x.Phone).NotEmpty().WithMessage("Phone cannot be empty");
+
+
+            RuleFor(x => x.AccessLevel).NotEmpty().NotNull().InclusiveBetween(1,5).WithMessage("AccessLevel cannot be empty");
+            RuleFor(x => x.AccessLevel).InclusiveBetween(1, 5).WithMessage("AccessLevel should be between 1 and 5");
+
+
+            RuleFor(x => x.Salary).NotEmpty().NotNull().WithMessage("Salary cannot be empty");
+            RuleFor(x => x.Salary).InclusiveBetween(5000, 50000).WithMessage("Salary should be between 5000 and 50000");
+
+
+        }
+    }
+}
